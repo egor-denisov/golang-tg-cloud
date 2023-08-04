f1ce92e348019b4ab7b84dbd65aef79d9e3178a2
Egor <egorbaraboshkin04@gmail.com>
2023-08-04 12:57:20 +0300

    uploud blank for environment

---
+++ environment/env.go
@@ -0,0 +1,13 @@
+package env
+
+// Database environment variables
+const (
+	HOST     = "localhost"
+	PORT     = 5432
+	USER     = "postgres"
+	PASSWORD = "********"
+	DBNAME   = "cloud"
+)
+
+// Telegram token
+const TOKEN = "23437....EE"
